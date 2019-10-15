aabec7ac2da3d71cd1f15af4e0bc76683e84ec60
Martins Ceske <Martinsh_Ceske@rcomext.com>
2019-10-15 17:15:55 +0300

    hw2 labots

--- src/lv/jg/lesson2/MathOperations.java
+++ src/lv/jg/lesson2/MathOperations.java
@@ -1,6 +1,7 @@
 
-package lv.jg.lesson2;
+package lv.jg.lesson2; //package rinda vienmēr ir pati pirmā klases failā
 import static java.lang.System.out;
+//OK, būtu pieticis tika ar int
 public class MathOperations {
     public static void main(String[] args) {
         int firstNumber = 25;
