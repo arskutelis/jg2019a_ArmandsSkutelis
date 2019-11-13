2d61aa364d25749900251a7376c82ea111fdb67a
Martins Ceske <Martinsh_Ceske@rcomext.com>
2019-11-13 15:14:18 +0200

    hw6 izlabots

--- src/lv/jg/lesson6/NumberService.java
+++ src/lv/jg/lesson6/NumberService.java
@@ -1,31 +1,39 @@
 package lv.jg.lesson6;
 
 public class NumberService {
+
+    //šai klasei nav jāsatur sevī stāvokļi, jo aprēķiniem katru reizi ir jāizmantoti jauni mainīgie
+    //tādēļ tos vajag definēt metodes līmenī
     private int firstNumber;
     private int secondNumber;
     private int sum = 0;
-    private int numberOfevens = 0;
+    private int numberOfevens = 0; //typo
 
     public NumberService(int firstNumber, int secondNumber) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
     }
 
+    //metode nosaukumi rakstāmi ar mazo burtu
     public int GetFirstNumber() {
         return firstNumber;
     }
 
+    //metode nosaukumi rakstāmi ar mazo burtu
     public int GetSecondNumber() {
         return secondNumber;
     }
 
+    //metodei ir jāatgriež rezultāts
     public void sumInRange() {
+        //kāds būs rezultāts, ja piermais skaitlis būs lielāks par otro
         for (firstNumber = GetFirstNumber(); firstNumber <= secondNumber; firstNumber++) {
-            sum = sum + firstNumber;
+            sum = sum + firstNumber; //rakstām īsāk: sum =+ firstNumber;
         }
         System.out.println("Sum of numbers in range is = " + sum + ".");
     }
 
+    //skatīt iepriekšējos komentārus
     public void getEvenNumberCount() {
         for (firstNumber = GetFirstNumber(); firstNumber <= secondNumber; firstNumber++) {
             if (firstNumber % 2 == 0) {
--- src/lv/jg/lesson6/PowerCalculator.java
+++ src/lv/jg/lesson6/PowerCalculator.java
@@ -1,6 +1,9 @@
 package lv.jg.lesson6;
 
 public class PowerCalculator {
+
+    //šeit arī neglabājam nekādu stāvokli
+    //visi mainīgie tikai metodēs
     private int number;
     private int power;
     private int result = 1;
@@ -16,11 +19,13 @@ public class PowerCalculator {
     public int getPower() {
         return power;
     }
+
+    //šai metodei ir jāpieņem mainīgie un jāatdod rezultāts
     public void pow() {
         while (power != 0) {
             power--;
-            result = result * number;
+            result = result * number; //rakstām īsāk result =* number;
         }
-        System.out.println("The result is " + result + ".");
+        System.out.println("The result is " + result + "."); //teksta izvadi var veikt testa klasē
     }
 }
